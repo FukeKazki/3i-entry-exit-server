a91652054d6ded5f55ace3e1ee34deb684513f12
FukeKazki <kazkichi0906@gmail.com>
2019-10-22 17:16:14 +0900

    Update: データベース接続用ルーティングの追加
    データベースクラスを呼び汎用的に使えるようにした
    時刻投稿ができるようにした
    ランキングの取得ができるようにした
    タイムログの取得ができるようにした

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import database from './database'
 const app = express()
 const PORT = process.env.PORT || 3000
 
@@ -14,16 +15,18 @@ app.use(express.urlencoded({
     extended: true
 }));
 
+// データベースの準備
+const db = new database
 
 /* ======================= */
 // ルーティング処理
 
-// 接続テスト用
-app.get('/', (req, res) => {
+// 接続テスト
+app.get('/', async (req, res) => {
     res.send('Hello World')
 })
 
-// 時刻投稿用
+// 時刻投稿
 app.post('/api/v1/post_time', (req, res) => {
     // 任意の値を取り出す
     const {
@@ -32,20 +35,25 @@ app.post('/api/v1/post_time', (req, res) => {
         time
     } = req.body
     // データベースへの登録を行う
-
+    db.timePost(user_handle, conditions, time)
     // レスポンス処理
-    console.log(user_handle, conditions, time)
-    res.json(req.body)
+    res.send("success")
 })
 
-// ランキング取得用
-app.get('/api/v1/ranking', (req, res) => {
+// ランキング取得
+app.get('/api/v1/ranking', async (req, res) => {
     // データベースからランキングリストを取得する
-
+    const result = await db.query('SELECT * FROM ranking')
     // ソートを行う
-
+    console.log(result)
     // レスポンス処理
     res.send('This is Ranking List')
+})
+
+// タイムログの取得
+app.get('/api/v1/time_log', async (req, res) => {
+    const result = await db.query('SELECT * FROM time_log')
+    res.send(result)
 })
 
 /* ======================= */
